6a2a2aed2bad5df02f16bf2f967de0ce1776b23f
Louis-Félix Tessier <louisfelix@gmail.com>
2020-02-06 16:36:40 -0500

    Checking against the empty string is more usual

--- azure.go
+++ azure.go
@@ -316,7 +316,7 @@ func (ac *AzureClient) getAzureMetricDefinitionResponse(resource string, metricN
 
 	metricsResource := fmt.Sprintf("subscriptions/%s%s", sc.C.Credentials.SubscriptionID, resource)
 	metricsTarget := fmt.Sprintf("%s/%s/providers/microsoft.insights/metricDefinitions?api-version=%s", sc.C.ResourceManagerURL, metricsResource, apiVersion)
-	if len(metricNamespace) > 0 {
+	if metricNamespace != "" {
 		metricsTarget = fmt.Sprintf("%s&metricnamespace=%s", metricsTarget, metricNamespace)
 	}
 
