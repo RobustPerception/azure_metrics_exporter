af9b645100d7fce0284d4cf592dfb3cad9bd57f1
Gotti <christian.gottinger@conplement.de>
2021-10-09 22:21:53 +0200

    feat: ensure metrics are called separate

--- azure.go
+++ azure.go
@@ -353,7 +353,7 @@ func (ac *AzureClient) getMetricNamespaces() (map[string]MetricNamespaceCollecti
 
 // Returns AzureMetricDefinitionResponse for a given resource
 func (ac *AzureClient) getAzureMetricDefinitionResponse(resource string, metricNamespace string) (*AzureMetricDefinitionResponse, error) {
-	apiVersion := "2018-01-01"
+	apiVersion := "2019-07-01"
 
 	metricsResource := fmt.Sprintf("subscriptions/%s%s", sc.C.Credentials.SubscriptionID, resource)
 	metricsTarget := fmt.Sprintf("%s/%s/providers/microsoft.insights/metricDefinitions?api-version=%s", sc.C.ResourceManagerURL, metricsResource, apiVersion)
@@ -626,7 +626,7 @@ type batchRequest struct {
 }
 
 func resourceURLFrom(resource string, metricNamespace string, metricNames string, aggregations []string, dimensions string) string {
-	apiVersion := "2018-01-01"
+	apiVersion := "2019-07-01"
 
 	path := fmt.Sprintf(
 		"/subscriptions/%s%s/providers/microsoft.insights/metrics",
@@ -655,6 +655,7 @@ func resourceURLFrom(resource string, metricNamespace string, metricNames string
 		Path:     path,
 		RawQuery: values.Encode(),
 	}
+
 	return url.String()
 }
 
